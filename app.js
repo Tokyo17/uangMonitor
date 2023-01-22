9326de1f2d833802c4fc1e754ea4cbf7ef6dabcc
mRezaMustofa <reza.mustofa@ist.id>
2023-01-22 07:36:28 +0700

    4

--- app.js
+++ app.js
@@ -14,45 +14,46 @@ app.use(
 
 
 const postgres = require('postgres');
-require('dotenv').config();
+// require('dotenv').config();
 
-const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
-const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
+// const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+// const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
 
-const sql = postgres(URL, { ssl: 'require' });
+// const sql = postgres(URL, { ssl: 'require' });
 
-async function getPostgresVersion() {
-  const result = await sql`select * from iot limit 2`;
-  console.log(result)
-return result
-}
+// async function getPostgresVersion() {
+//   const result = await sql`select * from iot limit 2`;
+//   console.log(result)
+// return result
+// }
 
 
-async function insertValue(temp,hum) {
-  const result = await sql`INSERT INTO iot (temperature,humidity) VALUES (${temp},${hum})`;
-  console.log(result)
-return result
-}
+// async function insertValue(temp,hum) {
+//   const result = await sql`INSERT INTO iot (temperature,humidity) VALUES (${temp},${hum})`;
+//   console.log(result)
+// return result
+// }
 
 
 
 
 
 app.get("/iot",function(req,res){
-  console.log(req.body.name)
-    getPostgresVersion()
-    .then(something => {
-      res.json(something)
-    });
+  // console.log(req.body.name)
+  res.json("success insert")
+    // getPostgresVersion()
+    // .then(something => {
+    //   res.json(something)
+    // });
 })
 
-app.post("/iot",function(req,res){
-  console.log(req.body.name)
-    insertValue(req.body.temp,req.body.hum)
-    .then(() => {
-      res.json("success insert")
-    });
-})
+// app.post("/iot",function(req,res){
+//   console.log(req.body.name)
+//     insertValue(req.body.temp,req.body.hum)
+//     .then(() => {
+//       res.json("success insert")
+//     });
+// })
 
 // const port = process.env.PORT || 3001;
 // app.listen(port,function(){
